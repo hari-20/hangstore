0c2e852532b16419546fc01555cfded89432f896
hari-20 <hariharsmrt@gmail.com>
2021-01-15 18:31:58 +0530

    fixed product overlay

---
+++ js/product-list.js
@@ -0,0 +1,24 @@
+const products = [
+    {
+        name : 'Esprit Ruffle Shirt',
+        tag : 'EspritRuffleShirt',
+        price : 18,
+        inCart : 0,
+        size : ['S','M','L','XL'],
+        colour: 'White',
+        description: "Women's Round Neck T-shirt",
+        category: 'women'
+    },
+    {
+        name : 'Herschel supply',
+        tag : 'Herschelsupply',
+        price : 35,
+        inCart : 0,
+        size : ['S','M','L','XL'],
+        colour: 'Half-White',
+        description: "Women's Casual shirt",
+        category: 'women'
+    }
+    ]
+    
+    
